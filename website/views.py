a76d50c51432ba856506565e9c1900d891ad568b
Estudo Kotlin Android <mr6227017@gmail.com>
2026-01-04 23:32:31 -0300

    Reorganização do projeto seguindo o formulário da aula e melhora de controle usando divisão do para melhor controle de pastas nomeadas de acordo com a utilidade

--- website/views.py
+++ website/views.py
@@ -2,30 +2,25 @@ from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
 from .models import Funcionario
 
-
 class IndexTemplateView(TemplateView):
-    template_name = "index.html"
-
+    template_name = "index.html" # Arquivo na raiz de templates
 
 class FuncionarioListView(ListView):
     model = Funcionario
     template_name = "lista.html"
     context_object_name = "funcionarios"
 
-
 class FuncionarioCreateView(CreateView):
     model = Funcionario
     template_name = "cria.html"
-    fields = '__all__'
+    fields = ['nome', 'sobrenome', 'cpf', 'tempo_de_servico', 'remuneracao']
     success_url = reverse_lazy("website:lista_funcionarios")
-
 
 class FuncionarioUpdateView(UpdateView):
     model = Funcionario
     template_name = "cria.html"
-    fields = '__all__'
+    fields = ['nome', 'sobrenome', 'cpf', 'tempo_de_servico', 'remuneracao']
     success_url = reverse_lazy("website:lista_funcionarios")
-
 
 class FuncionarioDeleteView(DeleteView):
     model = Funcionario
